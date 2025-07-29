3e4b644a77f1f1d106c18a83bb2a48be16bacae7
crocha3 <crocha3@wisc.edu>
2025-07-29 14:17:40 -0500

    Add files via upload

--- NearAxisApproximation.py
+++ NearAxisApproximation.py
@@ -1,13 +1,16 @@
 import jax.numpy as jnp
 import jax.scipy as jsp
+from scipy import stats
 
 
 L = jnp.pi
 nt = 100 
 nTheta = 200
-
-theta = jnp.linspace(-L, L, num = 100)
+deltaTheta = L/nTheta
+theta = jnp.linspace(-L-deltaTheta, L, num = 100)
 Theta = jnp.meshgrid(theta, ) 
+m = 1 # Mass placeholder
+T = 1 #Temperature placeholder
 
 #placeholder scalars
 kPsi = 1  
@@ -15,6 +18,8 @@ kAlpha = 1
 q = 1 #Charge
 mass = 1 
 vPerp = 1
+
+#The defined variables and gradients are placeholders to be replaced with the already created Equations 11-18 code
 
 def kappa(a):
     x,y,z = a
@@ -56,8 +61,32 @@ J0 = jsp.special.i0(besselArg)
 
 freqDrift = ((kPsi*eq16) + (kAlpha*eq17) + eq18) / freqCyclotron
 
+v = 1
+nV = 100
+vThe = 0.7
+vDe = 5
+vMine, vMaxe = -16*vThe, 16*vThe
+ve = jnp.linspace(vMine, vMaxe, nV)
+Theta, Ve = jnp.meshgrid(theta, ve, indexing= 'ij')
+waveNumberElectrons = 0.8
+Ae = 1
+n0e = 4
+
+F0 = n0e / jnp.linspace(2.*jnp.pi *vThe**2)*(0.5 * jnp.exp(-(Ve-vDe*vThe)**2 / (2.*vThe**2)) +0.5 * jnp.exp(-(Ve+vDe*vThe)**2 / (2.*vThe**2))) * (1 + Ae * jnp.sin(waveNumberElectrons * 2 * jnp.pi * Theta / L))
+magF0 = jnp.linalg.norm(F0)
+
+eta = 1 #Placeholder
+
+gradF0 = jnp.gradient(F0)
+den = m*freqCyclotron*T*magF0
+bCrossK = jnp.cross(b, kPerp)
+freqDiamagnetic = (jnp.dot(bCrossK, gradF0)) / den
+
+velDepFreqDiamagnetic = freqDiamagnetic*(1 + eta*((v**2 / vThe**2) - 3/2))
 
 
+vDrift = jnp.array([1,1,1]) #Replace this with cross product representation
+freqMagneticDrift = jnp.dot(kPerp, vDrift)
 
 
-
+Term = 
